c19b0d6f370b4224674befaf1ebd0d1f83b9d0e3
Alexander Korniichuk <world.kor.films@gmail.com>
2020-10-20 23:57:33 +0300

    first

---
+++ script.js
@@ -0,0 +1,38 @@
+let init = () => {
+
+    let main = (() => {
+        let btnSub = document.getElementById(`btn-Submit`);
+        let name = document.getElementById(`name`);
+        let surname = document.getElementById(`surname`);
+
+        btnSub.onclick = (event) => {
+            event.preventDefault();
+
+            let url = `process.php`;
+            let xhttp = new XMLHttpRequest();
+            
+
+            let data = {
+                name: name.value,
+                surname: surname.value
+            }
+
+            xhttp.onreadystatechange = () => {
+                if (xhttp.readyState == 4 && xhttp.status == 200) {
+                    alert(`Sucsess !`);
+                }
+            }
+            xhttp.onload = () => {
+                console.log(JSON.parse(xhttp.response));
+            }
+          
+            xhttp.open(`POST`, url, true);
+            xhttp.setRequestHeader(`Content-type`,`application/json`);
+            xhttp.responseType = `json`;
+            xhttp.send(JSON.stringify(data));
+        }
+
+    })();
+
+}
+window.addEventListener(`load`, init, false);
