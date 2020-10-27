7f80ea661af6f87704cd0069a149a52f8d65c9bb
STIG58 <stigua25@gmail.com>
2020-10-27 07:59:09 +0200

    Send response

--- ajax/firs/views.py
+++ ajax/firs/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 from django.views.decorators.http import require_POST
+import json
 
 def base(request):
 	return render(request, "firs/index.html")
 
 # @require_POST 
 def test(request):
+	
 	if request.method == 'POST':
 		if request.is_ajax():
-			return JsonResponse("Success")
-		else:
-			return HttpResponse("Request is not ajax")
+			received_json_data=json.loads(request.body)
+			received_json_data['name'] = 'other name'
+			received_json_data['surname'] = 'other surname'
+			response_data = json.dumps(received_json_data)
+			
+			response = JsonResponse(received_json_data)
+
+			return response
 	return HttpResponse("Request is not POST")
 	
